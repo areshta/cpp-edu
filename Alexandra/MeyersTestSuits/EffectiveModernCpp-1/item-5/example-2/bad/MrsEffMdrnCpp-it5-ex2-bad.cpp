10889d4cc8d0cc8e3de581476cf47171dbf368d2
Oleksii Reshta <alex.reshta@gmail.com>
2017-07-20 23:55:16 +0300

    working

--- Alexandra/MeyersTestSuits/EffectiveModernCpp-1/item-5/example-2/bad/MrsEffMdrnCpp-it5-ex2-bad.cpp
+++ Alexandra/MeyersTestSuits/EffectiveModernCpp-1/item-5/example-2/bad/MrsEffMdrnCpp-it5-ex2-bad.cpp
@@ -19,23 +19,26 @@ class MyString: public string
 {
 public:
     using string::string ;
-	string& getString(){ return *(static_cast<string *>(this)); }
 	~MyString () { cout << "destructor: " << *(static_cast<string *>(this)) << endl; } 
 };
 
+
 class hashing_func {
     public:
-        unsigned long operator()(const MyString& key) const {
-            unsigned long hash;
-            hash = std::hash<string>( *(static_cast<const string *>(&key)) );
+        unsigned long operator()(const MyString& key) const 
+		{
+			std::hash<std::string> hash_fn;
+			size_t hash = hash_fn(*(static_cast<const string *>(&key)));
             return hash;
         }
 };
 
 class key_equal_fn {
     public:
-        bool operator()(const MyString& t1, const MyString& t2) const {
-            return (t1 == t2);
+        bool operator()(const MyString& t1, const MyString& t2) const 
+		{
+			std::equal_to<std::string> eq_fn;
+			return eq_fn (*(static_cast<const string *>(&t1)), *(static_cast<const string *>(&t2)));
         }
 };
 
@@ -44,19 +47,11 @@ int32_t main()
 {
 	cout << "Effective Modern C++. The first edition. Item 5. Bad code." << endl;
 
-/*	unordered_map<MyString,int> someMap;
-
-
-	MyString s1 { string("One") };
-	MyString s2 { string("Two") };
-
-	someMap[s1] = 1;
-	someMap[s2] = 2;*/
-
 	unordered_map<MyString, int, hashing_func, key_equal_fn> someMap;
-	MyString s {"aaa"};
-	someMap[s] = 5;
-
+	MyString s1 {"one"};
+	MyString s2 {"two"};
+	someMap[s1] = 1;
+	someMap[s2] = 2;
 
 	return 0;
 
