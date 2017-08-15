e16cb8d5d7e59ae3fc428aa8023b3598d32e7ad0
Oleksii Reshta <alex.reshta@gmail.com>
2017-08-15 06:57:22 +0300

    Meyers Moders. Item-14. Example 1. Initial. Declare noexept

---
+++ MeyersTestSuits/EffectiveModernCpp-1/item-14/example-1/bad/MrsEffMdrnCpp-it14-ex1-bad.cpp
@@ -0,0 +1,24 @@
+/***************************************************************************************************/
+/* Examples created according items from Scott Meyers books for testing Static code analyzers      */
+/*                                                                                                 */ 
+/* Warnings: This code demonstrate using correponding item. Please ignore other possible warnings. */
+/*           c++14 standard is assumed                                                             */                  
+/***************************************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string sInfo =
+/*********************************************************************************************/
+	" Book:       Effective Modern C++. The first edition.          					\n"
+	" Item: #14.  Example 1. Declare functions noexcept if they won’t emit exceptions.  \n"
+	" Code type:  bad.                                             					\n\n" ;
+/*********************************************************************************************/
+
+int32_t main()
+{
+	cout << sInfo << endl;
+	return 0;
+}
