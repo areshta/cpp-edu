0ccba7e55f7aef3b64572dc686cdda2549439608
Oleksii Reshta <alex.reshta@gmail.com>
2017-08-21 23:15:34 +0300

    Meyers New. Item #15.  Use constexpr whenever possible. Initial.

---
+++ MeyersTestSuits/EffectiveModernCpp-1/item-15/example-1/bad/MrsEffMdrnCpp-it15-ex1-bad.cpp
@@ -0,0 +1,26 @@
+/***************************************************************************************************/
+/* Examples created according items from Scott Meyers books for testing Static code analyzers      */
+/*                                                                                                 */ 
+/* Warnings: This code demonstrate using correponding item. Please ignore other possible warnings. */
+/*           c++14 standard is assumed                                                             */                  
+/***************************************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string sInfo =
+/*********************************************************************************************/
+	" Book:       Effective Modern C++. The first edition.          					\n"
+	" Item: #15.  Example 1. Use constexpr whenever possible.                           \n"
+	" Code type:  bad.                                             					  \n\n" 
+/*********************************************************************************************/
+;
+
+int32_t main()
+{
+	cout << sInfo << endl;
+
+	return 0;
+}
