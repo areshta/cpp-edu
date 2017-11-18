62a481f24fdb45d9f15e4cf8127423172a6e283b
Oleksii Reshta <alex.reshta@gmail.com>
2017-11-18 19:49:55 +0200

    Item-23. Example -1

---
+++ MeyersTestSuits/EffectiveModernCpp-1/item-23/example-1/good/MrsEffMdrnCpp-it23-ex1-good.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************************************/
+/* Examples created according items from Scott Meyers books for testing Static code analyzers       */
+/*                                                                                                  */ 
+/* Warnings: This code demonstrate using corresponding item. Please ignore other possible warnings. */
+/*           c++14 standard is assumed                                                              */                  
+/* Author:   Oleksii Reshta	                                                                        */
+/****************************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::move;
+
+class MyString: public string
+{
+public:
+    using string::string ;
+    MyString( MyString&& ms): string(ms) { cout << "MyString Move constructor" << endl; }
+};
+
+MyString moveDemo()
+{
+    MyString ms {"moveDemo function"};
+    return move(ms); //OK: the move constructor will be called.
+}
+
+
+const string sInfo =
+/*****************************************************************************************************/
+	" Book:       Effective Modern C++. The first edition.                     			            \n"
+	" Item: #23.  Example 1. Understand std::move and std::forward                                  \n"
+	" Code type:  good.                                               			 	              \n\n" 
+/****************************************************************************************************/
+;
+
+
+int32_t main()
+{
+	cout << sInfo << endl;
+
+    MyString ms ( moveDemo() );
+    cout << "Get from function:" << static_cast<string>( ms ) << endl;
+
+	return 0;
+} 
