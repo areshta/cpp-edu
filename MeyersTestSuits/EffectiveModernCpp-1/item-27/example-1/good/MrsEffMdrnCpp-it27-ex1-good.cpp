f6e2fa10a5138f3aa1cdd7c81b3abcdcdbc20685
Oleksii Reshta <alex.reshta@gmail.com>
2017-11-22 23:24:55 +0200

    Item-27 draft

---
+++ MeyersTestSuits/EffectiveModernCpp-1/item-27/example-1/good/MrsEffMdrnCpp-it27-ex1-good.cpp
@@ -0,0 +1,109 @@
+/****************************************************************************************************/
+/* Examples created according items from Scott Meyers books for testing Static code analyzers       */
+/*                                                                                                  */ 
+/* Warnings: This code demonstrate using corresponding item. Please ignore other possible warnings. */
+/*           c++14 standard is assumed                                                              */                  
+/* Author:   Oleksii Reshta	                                                                        */
+/****************************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <set>
+#include <chrono>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::move;
+using std::multiset;
+using std::forward;
+using std::vector;
+
+const string sInfo =
+/*****************************************************************************************************/
+	" Book:       Effective Modern C++. The first edition.                     			            \n"
+	" Item: #27.  Example 1. Familiarize yourself with alternatives to                              \n"
+    "                        overloading on universal references                                    \n"
+	" Code type:  good.                                               			 	              \n\n" 
+/****************************************************************************************************/
+;
+
+
+
+class SomeClass 
+{
+public:
+    template
+    < 
+        typename T,
+        typename =  std::enable_if_t
+                <
+                    !std::is_base_of<Person, std::decay_t<T>>::value
+                    &&
+                    !std::is_integral<std::remove_reference_t<T>>::value
+                >
+    >
+    explicit Person(T&& n)
+: name(std::forward<T>(n))
+{
+// assert that a std::string can be created from a T object
+static_assert(
+std::is_constructible<std::string, T>::value,
+"Parameter n can't be used to construct a std::string"
+);
+… // the usual ctor work goes here
+}
+… // remainder of Person class (as before)
+};
+
+multiset<string> gSomeData;
+
+using logtm = std::chrono::time_point<std::chrono::system_clock>; 
+
+void log(const logtm& tm, const string& st)
+{
+	std::time_t now_c = std::chrono::system_clock::to_time_t(tm);
+	cout << now_c << " : " << st << endl;
+}
+
+const string& nameFromIdx(size_t idx)
+{
+	static vector<string> svData { "Non", "Ann", "Al", "Bill", "Kat", "Willy" };
+	if( idx >= svData.size() )
+	{
+		idx = 0;
+	}
+	
+	return svData[idx];
+}
+
+template<typename T>
+void logAndAdd(T&& sData)
+{
+	auto now = std::chrono::system_clock::now();
+	log(now, sData);
+	gSomeData.emplace(forward<T>(sData));
+}
+
+void logAndAdd(size_t idx) 
+{
+	auto now = std::chrono::system_clock::now();
+	string sData = nameFromIdx(idx);
+	log(now, sData);
+	gSomeData.emplace(sData);
+}
+
+int32_t main()
+{
+	cout << sInfo << endl;
+
+	string sName("John"); 
+	logAndAdd(sName);
+	logAndAdd(string("Peppy")); 
+	logAndAdd("Abdula"); 
+	logAndAdd( static_cast<size_t>(1) );
+	
+	return 0;
+} 
