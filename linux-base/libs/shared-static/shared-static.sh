e19cd17112082bd23dc9614baf1e32d071399ed2
Alexey Reshta <areshta@luxoft.com>
2018-07-17 18:26:21 +0300

    so lib example added

--- linux-base/libs/shared-static/shared-static.sh
+++ linux-base/libs/shared-static/shared-static.sh
@@ -4,11 +4,21 @@ set -x
 rm -f *.out
 rm -f *.o
 rm -f lib*.*
+rm *.so
+
 
 c++ -Wall -fPIC -c inputer.cpp
-c++ -Wall -fPIC c summator.cpp
+c++ -Wall -fPIC -c summator.cpp
 
 gcc -shared -Wl,-soname,libctest.so.1 -o libctest.so.1.0   *.o
+
+ln -s ./libctest.so.1.0 ./libctest.so.1 
+ln -s ./libctest.so.1 ./libctest.so
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
+g++ -Wall  -L./ main.cpp -lctest -o main 
+
+./main
 
 #c++ main.cpp libctest.a -o main
 
