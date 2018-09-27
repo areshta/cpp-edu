4b96093282a6f8972d25cf8e4abf5a28208aac2f
Alexey Reshta <areshta@luxoft.com>
2018-09-27 09:00:38 +0300

    enum array minor update

--- some_cpp_examples/enum_array/CppApplication_1/enum_array.cpp
+++ some_cpp_examples/enum_array/CppApplication_1/enum_array.cpp
@@ -21,6 +21,7 @@ enum En3 {a3 = 3 , b3 = 2, c3 = 1, d3 = 0 };
 enum En4 {a4, b4, c4, d4, e4, f4 = 20};
 enum En5 {a5};
 enum En6 {a6, b6, c6, d6, e6, f6, g6, h6, i6, j6, k6};
+enum En7 {a7=-10, b7, c7=-1, d7, e7};
 
 template<typename... Args>
 constexpr int enum_size(Args... args)
@@ -40,10 +41,36 @@ constexpr int enum_summ(T first, Args... args)
    return first + enum_summ(args...);
 }
 
+template<typename T>
+constexpr bool  enum_no_negative(T x)
+{
+   return x >= 0;
+}
+
+
+template<typename T, typename... Args>
+constexpr bool enum_no_negative(T first, Args... args)
+{
+   return (first >= 0) && enum_no_negative(args...);
+}
+
+template<typename T>
+constexpr int  enum_min(T x)
+{
+   return x >= 0;
+}
+
+
+template<typename T, typename... Args>
+constexpr int enum_min(T first, Args... args)
+{
+   return first < enum_min(args...) ? first : enum_min(args...);
+}
+
 template<typename... Args>
 constexpr bool enumIsArrayable(Args... args)
 {
-   return enum_summ(args...) - enum_size(args...)*(enum_size(args...)-1)/2 == 0;
+   return enum_no_negative(args...) && (enum_summ(args...) - enum_size(args...)*(enum_size(args...)-1)/2 == 0);
 }
 
 #define listEnum1 a1, b1, c1, d1, e1, f1
@@ -64,13 +91,20 @@ typedef std::conditional<enumIsArrayable(__VA_ARGS__) , \
  
 int main(int argc, char** argv) {
 
+   constexpr int min_value = enumIsArrayable(a7, b7, c7, d7, e7);
+   
+   cout << "No negative " << enum_no_negative(a7, b7, c7, d7, e7) << endl;
+   cout << "Enum min a7...e7 " << enum_min(a7, b7, c7, d7, e7)<< endl;
+   
    cout << "size = " << enum_size(a,b,c,d,e)<< endl;
    cout << "summ = " << enum_summ(a,b,c,d,e)<< endl;
    cout << "Good: isArrayable a...e " << enumIsArrayable(a,b,c,d,e)<< endl;
    
    cout << "Good: isArrayable a1...f1 " << enumIsArrayable(a1, b1, c1, d1, e1, f1)<< endl;
    cout << "Bad: isArrayable a2...d2 " << enumIsArrayable(a2, b2, c2, d2)<< endl;
-   cout << "Good: isArrayable a2...d2 " << enumIsArrayable(a3, b3, c3, d3)<< endl;
+   cout << "Good: isArrayable a3...d3 " << enumIsArrayable(a3, b3, c3, d3)<< endl;
+   cout << "Bad: isArrayable a7...e7 " << enumIsArrayable(a7, b7, c7, d7, e7)<< endl;
+   cout << "Bad: isArrayable a7...e7 " << min_value << endl;
    
    // solution using conditional typedef
    
