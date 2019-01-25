9c4a90081f760d76c24362910444c6b7521c1f20
Alex Reshta <alex.reshta@gmail.com>
2019-01-25 10:29:27 +0200

    Minor update

--- cpp_issues/cpp_issues.cpp
+++ cpp_issues/cpp_issues.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void out(double a, double b)
-{
-	cout << "\n150 + 150 = " << a + b << " ?!!!   :-(" << endl;
-}
 
 int main() 
 {
     //overflow issue
-	int8_t a = 150, b=150;
-	cout << "\n150 + 150 = " << a + b << " ?!!!   :-(" << endl;
+	uint8_t a = 150, b=150;
+    uint8_t c = a+b;
+	cout << "\n150 + 150 = " << static_cast<int>(c) << " ?!!!   :-(" << endl;
     cout << "\n150 + 150 = " << static_cast<int16_t>(a) + static_cast<int16_t>(b) << " ???" << endl;
-	float c = 1.*a;
+	float cc = 1.*a;
 	float d = 1.*b;
-	cout << "\n150 + 150 = " << c + d  << " ???" << endl;
+	cout << "\n150 + 150 = " << cc + d  << " ???" << endl;
 	double e = 1.*static_cast<double>(a);
 	double f = 1.*static_cast<double>(b);
 	cout << "\n150 + 150 = " << e + f  << " ???" << endl;
     double r = e+f;
 	cout << "\n150 + 150 = " << r  << " ???" << endl;
-
-    out(e,f);
 
     return 0;
 }
--- some_cpp_examples/raii-straustrup/exec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-g++ -std=c++14 *.cpp
-./a.out
-rm -f ./a.out
