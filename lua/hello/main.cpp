943b3c80aeda3e4a1e96d1ccc63299754698a6bf
Alexey Reshta <areshta@luxoft.com>
2020-05-25 17:02:30 +0300

    lua example

---
+++ lua/hello/main.cpp
@@ -0,0 +1,38 @@
+// Lua 5.3.3
+#include <iostream>
+
+extern "C" {
+    #include "lua.h"
+    #include "lualib.h"
+    #include "lauxlib.h"
+}
+
+using namespace std;
+
+int LuaOutputToCpp(lua_State *luaVM)
+{
+    string strSource = lua_tostring(luaVM, 1);
+    cout << "from_lua: " << strSource << endl;
+}
+
+int main(int argc, char *argv[])
+{
+
+    lua_State *l = luaL_newstate();
+    if(l != nullptr){
+        luaL_openlibs(l);
+        lua_register(l, "LuaOutputToCpp", LuaOutputToCpp);
+        int ret = luaL_dofile(l,"/home/alexey/working/cpp-edu/lua/hello/script.lua");
+        if(ret > 0){
+            cout << "lua exec error\n";
+        }
+
+        lua_getglobal(l,"print_str");
+        lua_pushstring(l,"lua print_str from cpp");
+        lua_pcall(l, 1, 0, 0);
+
+        lua_close(l);
+    }
+
+    return 0;
+}
---
+++ lua/hello/script.lua
@@ -0,0 +1,6 @@
+function print_str(str)
+    print(str)
+end
+print ("Hello World")
+LuaOutputToCpp("Call cpp-fun")
+print_str("print_str from Lua")
